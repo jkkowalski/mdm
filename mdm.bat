6b5dbf7bd75fb036cdfec79f7a83d99d04990472
Kuba Kowalski <qbq@qbq.pl>
2026-06-10 08:53:30 +0200

    Inicjalizacja srodowiska przed uruchomieniem pythona

--- mdm.bat
+++ mdm.bat
@@ -1,2 +1,10 @@
-@echo off
-python "%~dp0mdm.py" %*
+@echo off
+setlocal
+if exist "%~dp0venv\Scripts\python.exe" (
+    "%~dp0venv\Scripts\python.exe" "%~dp0mdm.py" %*
+) else if exist "%~dp0.venv\Scripts\python.exe" (
+    "%~dp0.venv\Scripts\python.exe" "%~dp0mdm.py" %*
+) else (
+    python "%~dp0mdm.py" %*
+)
+endlocal
